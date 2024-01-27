bc44b619a62586447383b10796b9817e287bbc7a
liuglaze <1823892985@qq.com>
2024-01-27 21:44:32 +0800

    循环

--- BonAppetit/Assets/Scripts/GamePlay/Player.cs
+++ BonAppetit/Assets/Scripts/GamePlay/Player.cs
@@ -6,7 +6,7 @@ using static UnityEngine.GraphicsBuffer;
 public class Player : MonoBehaviour
 {
     public MicroRecord microRecord;
-    public float minVolume=20f;
+    public float minVolume=10f;
     public float canElimateVolume = 30f;
     public float minPosition = -4f;
     public float maxPosition = 4f;
@@ -16,6 +16,7 @@ public class Player : MonoBehaviour
     public float returnSpeed = 1f;
     public Vector2 centerPoz= Vector2.zero;
     public float targetY;
+    public float time = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,15 +29,26 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (time>0f)
+        {
+            time -= Time.deltaTime;
+        }
+        else
+        {
+            GamePlayUIManager.Instance.OpenSettlePanel();
+        }
+        
+            
+        
         if (microRecord != null && microRecord.volume > minVolume)
         {
             targetY = Conversion(microRecord.pitch);
-            transform.position = Vector2.Lerp(transform.position, new Vector2(0f, targetY), Time.deltaTime * returnSpeed);
+            transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x, targetY), Time.deltaTime * returnSpeed);
         }
         else
         {
             targetY = minPosition;
-            transform.position = Vector2.Lerp(transform.position, new Vector2(0f, targetY), Time.deltaTime * returnSpeed);
+            transform.position = Vector2.Lerp(transform.position, new Vector2(transform.position.x, targetY), Time.deltaTime * returnSpeed);
         }
     }
     public bool canEliminate()
